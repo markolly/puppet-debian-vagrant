32f996729146cb410675e752bdd0af1061d7335b
markolly <mark@oliver.cx>
2018-11-01 15:48:39 +0000

    Added tests for ssh class

--- spec/classes/ssh_spec.rb
+++ spec/classes/ssh_spec.rb
@@ -3,12 +3,43 @@ describe 'vagrant::ssh' do
   let(:facts) do
     {
       kernel: 'Linux',
-      operatingsystem: 'Debian',
-      operatingsystemmajrelease: '9',
-      operatingsystemrelease: '9',
+      operatingsystem: 'Ubuntu',
+      operatingsystemmajrelease: '16.04',
+      operatingsystemrelease: '16.04',
       osfamily: 'Debian'
     }
   end
   it { should contain_class('vagrant::ssh') }
   it { should compile }
+  it { should contain_file('/etc/ssh/sshd_config') }
+  it do 
+  	should contain_file_line('Configure UseDNS in SSH daemon').with(
+      :path   => '/etc/ssh/sshd_config',
+      :line   => 'UseDNS no',
+      :match  => '^UseDNS'
+    )
+  end
+  it do 
+  	should contain_file_line('Configure GSSAPIAuthentication in SSH daemon').with(
+      :path   => '/etc/ssh/sshd_config',
+      :line   => 'GSSAPIAuthentication no',
+      :match  => '^GSSAPIAuthentication'
+    )
+  end
+  it do
+    should contain_file('/home/vagrant/.ssh').with(
+      ensure: 'directory',
+      owner: 'vagrant',
+      group: 'vagrant',
+      mode: '0700',
+    )
+  end
+  it do
+    should contain_file('/home/vagrant/.ssh/authorized_keys').with(
+      ensure: 'present',
+      owner: 'vagrant',
+      group: 'vagrant',
+      mode: '0600',
+    )
+  end
 end
