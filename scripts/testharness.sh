9b24db3450c9588b03223bdccfdbfbdb6b4a91c8
markolly <mark@oliver.cx>
2018-11-02 15:55:26 +0000

    Refactored testharness

--- scripts/testharness.sh
+++ scripts/testharness.sh
@@ -5,47 +5,47 @@ STASH_NAME="pre-commit-$(date +%s)"
 
 # FUNCTIONS
 function log {
-	printf "\n--- $1 ---\n"
+  printf "\n--- $1 ---\n"
 }
 
 function gitStash {
-	# Stash unstaged changes before running tests
-	git stash save -q --keep-index $STASH_NAME
-	if [ "$?" -ne 0 ]; then
-				log "GIT STASH FAILED"
-				exit 1
-	fi
+  # Stash unstaged changes before running tests
+  git stash save -q --keep-index $STASH_NAME
+  if [ "$?" -ne 0 ]; then
+    log "GIT STASH FAILED"
+    exit 1
+  fi
 }
 
 function gitPopStash {
-	FIND_STASH=$(git stash list | grep $STASH_NAME)
-	if [ "$FIND_STASH" ]; then
-		git stash pop -q stash@{0}
-			if [ "$?" -ne 0 ]; then
-		    		log "GIT STASH POP FAILED"
-		    		exit 1
-			fi
-	fi
+  FIND_STASH=$(git stash list | grep $STASH_NAME)
+  if [ "$FIND_STASH" ]; then
+    git stash pop -q stash@{0}
+    if [ "$?" -ne 0 ]; then
+      log "GIT STASH POP FAILED"
+      exit 1
+    fi
+  fi
 }
 
 function rubocopTests {
-	log "RUNNING RUBOCOP TESTS"
-	rake rubocop
-	if [ "$?" -ne 0 ]; then
-    		log "RUBOCOP TESTS FAILED"
-				gitPopStash
-    		exit 1
-	fi
+  log "RUNNING RUBOCOP TESTS"
+  rake rubocop
+  if [ "$?" -ne 0 ]; then
+    log "RUBOCOP TESTS FAILED"
+      gitPopStash
+      exit 1
+  fi
 }
 
 function specTests {
-	log "RUNNING RELEASE CHECKS"
-	rake release_checks
-	if [ "$?" -ne 0 ]; then
-    		log "RELEASE CHECKS FAILED"
-				gitPopStash
-    		exit 1
-	fi
+  log "RUNNING RELEASE CHECKS"
+  rake release_checks
+  if [ "$?" -ne 0 ]; then
+    log "RELEASE CHECKS FAILED"
+    gitPopStash
+    exit 1
+  fi
 }
 
 # PROCESS
